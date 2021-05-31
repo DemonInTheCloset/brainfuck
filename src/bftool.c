3cfc8d0d4a17b2ab878a3df523a632306caa6dbd
DemonInTheCloset <demoninthecloset@gmail.com>
2021-05-31 13:02:27 +0200

    Implemented reading of files

--- src/bftool.c
+++ src/bftool.c
@@ -10,6 +10,48 @@ static const char usage[] = "usage: %s [-s] FILE"
                             "\n\t-s:   Interpret FILE as a Brainfuck program"
                             "\n";
 
+size_t read_file(const char *path, char **output) {
+  // Adapted from:
+  // https://stackoverflow.com/questions/174531/how-to-read-the-content-of-a-file-to-a-string-in-c
+  size_t n = 0;
+  char *buffer = NULL;
+  FILE *file = fopen(path, "r");
+
+  if (!file) {
+    fprintf(stderr, "Error: couldn't open \"%s\"\n", path);
+  }
+
+  if (fseek(file, 0, SEEK_END)) {
+    fprintf(stderr, "Error: Failed to seek to EOF\n");
+    fclose(file);
+    return 0;
+  }
+  n = ftell(file);
+  if (fseek(file, 0, SEEK_SET)) {
+    fprintf(stderr, "Error: Failed to seek to start of file\n");
+    fclose(file);
+    return 0;
+  }
+
+  buffer = malloc(n);
+  if (!buffer) {
+    fprintf(stderr, "Error: Failed to allocate %luB buffer\n", n);
+    fclose(file);
+    return 0;
+  }
+
+  size_t read = fread(buffer, 1, n, file);
+  fclose(file);
+  if (read != n) {
+    fprintf(stderr, "Warning: Read %luB/%luB\n", read, n);
+    n = read;
+  }
+
+  *output = buffer;
+
+  return n;
+}
+
 int main(int argc, char **argv) {
   bool is_file = true;
   int opt;
@@ -39,7 +81,35 @@ int main(int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  printf("Is file: %d\n", is_file);
-  printf("FILE: \"%s\"\n", argv[optind]);
+  char *file = argv[optind];
+
+  char *text = NULL;
+  size_t len = 0;
+
+  if (is_file) {
+    len = read_file(file, &text);
+  } else {
+    text = file;
+    len = strlen(file);
+  }
+
+  if (!len) {
+    printf("Program of length 0 given; nothing to do\n");
+    return EXIT_SUCCESS;
+  }
+
+  uint8_t *buffer = malloc(sizeof(uint8_t) * len);
+
+  if (!buffer) {
+    fprintf(stderr, "Failed to allocate %luB buffer\n", sizeof(uint8_t) * len);
+    return EXIT_FAILURE;
+  }
+
+  len = proccess(len, text, buffer);
+
+  pretty_print(len, buffer, " ");
+
+  free(buffer);
+
   return EXIT_SUCCESS;
 }
