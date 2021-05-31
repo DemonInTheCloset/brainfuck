4f75a778c99dfaaa6b081e785679b28efa362716
DemonInTheCloset <demoninthecloset@gmail.com>
2021-05-31 14:06:19 +0200

    Fixed value extraction not signextending properly

--- src/bfclean.h
+++ src/bfclean.h
@@ -9,6 +9,6 @@ typedef enum { BF_ADD = 0x0, BF_PTR = 0x1, BF_JMP = 0x2, BF_IOC = 0x3 } BF_OPT;
 
 static inline BF_OPT instr_opt(uint8_t instr) { return instr & 0x3; }
 
-static inline int instr_imm(int8_t instr) { return instr / 4; }
+static inline int instr_imm(int8_t instr) { return instr >> 2; }
 
 size_t proccess(size_t n, const char prog[n], uint8_t output[n]);
